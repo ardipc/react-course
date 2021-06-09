cda4ecc58db9b05f154f101580feb06285b8d0d2
Ahmad Ardiansyah <ardiansyah3ber@gmail.com>
2021-06-09 22:21:59 +0700

    footer

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -6,13 +6,13 @@ export default function Footer() {
         <div className="row align-items-center no-gutters border-top py-2">
           {/* Desc */}
           <div className="col-md-6 col-12 text-center text-md-left">
-            <span>© 2021 Geeks. All Rights Reserved.</span>
+            <span>© 2021 Ardimasyu. All Rights Reserved.</span>
           </div>
           {/* Links */}
           <div className="col-12 col-md-6">
             <nav className="nav nav-footer justify-content-center justify-content-md-end">
-              <a className="nav-link active pl-0" href="#!">Privacy</a>
-              <a className="nav-link" href="#!">Terms </a>
+              <a className="nav-link active pl-0" href="#!">Privasi</a>
+              <a className="nav-link" href="#!">Ketentuan </a>
               <a className="nav-link" href="#!">Feedback</a>
               <a className="nav-link" href="#!">Support</a>
             </nav>
